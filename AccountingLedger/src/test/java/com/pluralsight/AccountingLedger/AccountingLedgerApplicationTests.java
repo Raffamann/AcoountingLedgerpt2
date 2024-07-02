870bfdfc05b68df6d08da952acca9e9d1f52f49b
Raffamann <rafael.millaremanan@gmail.com>
2024-07-02 15:19:15 -0400

    second commit

---
+++ AccountingLedger/src/test/java/com/pluralsight/AccountingLedger/AccountingLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.AccountingLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
